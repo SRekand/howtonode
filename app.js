7b93c7dda53b9b6fe92affe2fd7b5d2660a564fb
Steven Kristjan Rekand <s.rekand@khk.ee>
2022-01-27 23:30:47 +0200

    Rename filmes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
+fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
     if (err) throw err;
-    console.log('Updated!');
+    console.log('File Renamed!');
 });
