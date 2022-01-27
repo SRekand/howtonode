c09617984b95cbfb744fd56dbd026479cca9ed77
Steven Kristjan Rekand <s.rekand@khk.ee>
2022-01-27 23:46:50 +0200

    Node Evemts

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-var http = require('http');
-var uc = require('upper-case');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(uc.upperCase("Hello World!"));
-    res.end();
-}).listen(8080);
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+    console.log('I hear a scream!');
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
