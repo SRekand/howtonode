88eae97b216a754a0dec1376767cee4e626a05f3
Steven Kristjan Rekand <s.rekand@khk.ee>
2022-01-27 23:29:42 +0200

    Update files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
     if (err) throw err;
-    console.log('Saved!');
+    console.log('Updated!');
 });
